7718f39d3b2209cd3b438e3e57abff38c47e30bf
asma ezzati <asmaezzati2019@gmail.com>
2025-09-05 19:07:20 +0330

    Provide store

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route/router.jsx";
 
+import { Provider } from "react-redux";
+import { store } from "./store/index.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
