6b2939af568604b84fac683f36836afc543ab3bc
asma ezzati <asmaezzati2019@gmail.com>
2025-09-05 19:15:59 +0330

    add thunk & fetch data

--- src/reducer/BookSlice.js
+++ src/reducer/BookSlice.js
@@ -1,9 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "http://localhost:9000/books";
 
 const initialState = {
   items: [],
   status: "idle",
 };
+
+export const fetchBooks = createAsyncThunk("books/fetchBooks", async () => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export const booksSlice = createSlice({
   name: "books",
