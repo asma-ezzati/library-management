ed210be0390b8f595df94a150eb55bece0b38cce
asma ezzati <asmaezzati2019@gmail.com>
2025-09-06 19:44:49 +0330

    style : add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    colors: {
+      pink1: "#EDAFB8",
+      pink2: "#F7E1D7",
+      green1: "#DEDBD2",
+      green2: "#B0C4B1",
+      green3: "#4A5759",
+    },
   },
   plugins: [],
-}
-
+};
