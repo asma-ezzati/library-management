01fa113e6952b12b2b7a6f8fa7beb4a7db1399ad
asma ezzati <asmaezzati2019@gmail.com>
2025-09-07 18:29:51 +0330

    style modified search bar

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,10 +12,10 @@ const Banner = () => {
           <form className="my-2  ">
             <input
               placeholder="جستجو"
-              className="w-80 bg-pink2 text-green3 font-Vazir  px-5 py-2 border-4 border-pink1 rounded-full 
+              className="w-80 bg-pink2 text-green3 font-Vazir  px-5 py-2 border-2 border-pink1 rounded-full 
               transition-transform duration-1000
-              focus:outline-none focus:border-4 focus:border-pink1 focus:transition-transform  focus:duration-1000  
-              focus:scale-x-125  focus:-translate-y-10"
+              focus:outline-none focus:border-2 focus:border-pink1 focus:transition-transform  focus:duration-1000  
+              focus:scale-x-125  focus:-translate-y-5"
             ></input>
           </form>
         </div>
