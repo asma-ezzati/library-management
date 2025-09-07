875ecd14a62a7770c82ff220af6b1d9a16b579f1
asma ezzati <asmaezzati2019@gmail.com>
2025-09-07 14:22:06 +0330

    style: implement Search Bar

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,22 @@ import banner from "../assets/banner-pic.jpg";
 const Banner = () => {
   return (
     <>
-      <div className="w-[100%] h-auto ">
-        <img src={banner} className="w-full h-full object-contain "></img>
+      <div className="w-[100%] h-auto relative ">
+        <img
+          src={banner}
+          className="w-full h-full object-contain blur-[2px] "
+        ></img>
+        <div className="absolute inset-0 flex items-center justify-center ">
+          <form className="my-2  ">
+            <input
+              placeholder="جستجو"
+              className="w-80 bg-pink2 text-green3 font-Vazir  px-5 py-2 border-4 border-pink1 rounded-full 
+              transition-transform duration-1000
+              focus:outline-none focus:border-4 focus:border-pink1 focus:transition-transform  focus:duration-1000  
+              focus:scale-x-125  focus:-translate-y-10"
+            ></input>
+          </form>
+        </div>
       </div>
     </>
   );
