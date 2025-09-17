3c126090dac939f69291fdd7e42e930194c70375
asma ezzati <asmaezzati2019@gmail.com>
2025-09-17 16:20:42 +0330

    fix bug in json server

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -15,32 +15,35 @@ const Books = () => {
       </h1>
       <div className="flex justify-around flex-row ">
         {lastThree.map((book) => (
-          <div
-            key={book.id}
-            className="w-[300px] border-2 rounded-3xl  border-green2 bg-green1  mx-5 my-3 duration-200
+          <>
+            <img className="w-10 h-11" src={book.image} alt={book.title}></img>
+            <div
+              key={book.id}
+              className="w-[300px] border-2 rounded-3xl  border-green2 bg-green1  mx-5 my-3 duration-200
             hover:transition-transform hover:-translate-y-2 hover:duration-200 "
-          >
-            <h2 className=" font-Vazir text-center text-green3 font-bold py-3 pt-6  ">
-              {book.title}
-            </h2>
-            <hr className="border-green3 border-t-2 w-1/2 mx-auto rounded-full"></hr>
-            <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-              {book.author}
-            </h4>
-            <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-              {book.genre}
-            </h4>
-            <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-              {book.price}
-            </h4>
-            <Link to={`/books/${book.id}`}>
-              <IoGlassesOutline
-                size={50}
-                className="rounded-full border-2 p-2 m-2 border-green3 bg-green3 text-pink2 
+            >
+              <h2 className=" font-Vazir text-center text-green3 font-bold py-3 pt-6  ">
+                {book.title}
+              </h2>
+              <hr className="border-green3 border-t-2 w-1/2 mx-auto rounded-full"></hr>
+              <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                {book.author}
+              </h4>
+              <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                {book.genre}
+              </h4>
+              <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                {book.price}
+              </h4>
+              <Link to={`/books/${book.id}`}>
+                <IoGlassesOutline
+                  size={50}
+                  className="rounded-full border-2 p-2 m-2 border-green3 bg-green3 text-pink2 
                 hover:bg-pink2 hover:text-green3 "
-              />
-            </Link>
-          </div>
+                />
+              </Link>
+            </div>
+          </>
         ))}
       </div>
 
