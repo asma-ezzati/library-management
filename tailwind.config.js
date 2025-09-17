c395458c1811ab13eea7cd127cbef464cb77009a
asma ezzati <asmaezzati2019@gmail.com>
2025-09-17 18:05:59 +0330

    style: add color white in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      white: "#fffcf2",
       pink1: "#EDAFB8",
       pink2: "#F7E1D7",
       green1: "#DEDBD2",
