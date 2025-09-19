f3692f0b8f63650388d2f9cd632ee9560b417cfc
asma ezzati <asmaezzati2019@gmail.com>
2025-09-19 13:16:28 +0330

    implement route for signup page

--- src/route/router.jsx
+++ src/route/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import SignupPage from "../components/SignupPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "signup",
+    element: <SignupPage />,
   },
 ]);
