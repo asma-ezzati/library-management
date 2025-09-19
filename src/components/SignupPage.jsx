59c2d49df2e079cd00df5a4648c1a8d76c3ff910
asma ezzati <asmaezzati2019@gmail.com>
2025-09-19 14:11:39 +0330

    feat; add signup page

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,4 +1,21 @@
 const SignupPage = () => {
-  return <h1>Signup Page</h1>;
+  return (
+    <>
+      <div className="card">
+        <div className="hero">
+          <div className="hero-inner">
+            <img></img>
+          </div>
+        </div>
+        <form>
+          <h2>signup</h2>
+          <h3>signup and have a account</h3>
+          <input type="email" placeholder="Email"></input>
+          <input type="password" placeholder="password"></input>
+          <button type="submit">signup</button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default SignupPage;
