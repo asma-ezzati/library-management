951a094092004a6325e43ab9246393f15ba15a4c
asma ezzati <asmaezzati2019@gmail.com>
2025-09-19 17:38:15 +0330

    style: signup-page

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,20 +1,54 @@
+import { Link } from "react-router-dom";
+import signupPic from "../assets/signup-pic.jpg";
+
 const SignupPage = () => {
   return (
     <>
-      <div className="card">
-        <div className="hero">
-          <div className="hero-inner">
-            <img></img>
+      <main className="min-h-screen bg-pink2 flex items-center place-content-center ">
+        <div className="flex items-center bg-white border-2 border-pink1 rounded-[16px] p-[6px] w-[90vw] shadow-md ">
+          <div className="p-[32px] w-[50%] ">
+            <h2 className="font-Vazir m-0 font-[32px] mb-[20px] "></h2>
+            <h3 className="font-Vazir m-0  font-[14px] mb-[40px]  "></h3>
+            <form>
+              <input
+                type="email"
+                placeholder="Email"
+                className=" w-[100%] bg-white border-2 border-pink2 mb-[10px] border-1 py-[16px] font-Vazir block rounded-[6px] h-[48px] font-[16px] p-1 focus:outline-pink1  "
+              ></input>
+              <input
+                type="password"
+                placeholder="Password"
+                className=" w-[100%]  bg-white border-2 border-pink2 mb-[10px] border-1 py-[16px] font-Vazir block rounded-[6px] h-[48px] font-[16px] p-1  focus:outline-pink1 "
+              ></input>
+              <div className="flex items-center justify-between my-[40px]">
+                <label className="checkbox">
+                  <input type="checkbox" className="accent-pink1"></input>
+                  <span className="check font-Vazir text-green3 font-semibold">
+                    {" "}
+                    Remember Me
+                  </span>
+                </label>
+                <a className="font-Vazir text-green3 font-semibold">
+                  Forget password
+                </a>
+              </div>
+              <button className="bg-pink1 text-white px-[48px] py-[10px] rounded-[6px]  transition duration-75 hover:text-pink1 hover:bg-pink2 hover:transition  hover:duration-75">
+                Sign In
+              </button>
+              <Link
+                to={"/"}
+                className="bg-pink1 mx-3 text-white px-[48px] py-[10px] rounded-[6px]  transition duration-75 hover:text-pink1 hover:bg-pink2 hover:transition  hover:duration-75"
+              >
+                Cancel
+              </Link>
+            </form>
           </div>
+          <img
+            src={signupPic}
+            className="w-[50%] border-2 border-pink1 rounded-[14px] self-stretch object-cover m-5  "
+          ></img>
         </div>
-        <form>
-          <h2>signup</h2>
-          <h3>signup and have a account</h3>
-          <input type="email" placeholder="Email"></input>
-          <input type="password" placeholder="password"></input>
-          <button type="submit">signup</button>
-        </form>
-      </div>
+      </main>
     </>
   );
 };
