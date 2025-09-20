a09615a73aeff90f9a36d89e37176d8ed43080ab
asma ezzati <asmaezzati2019@gmail.com>
2025-09-20 15:26:18 +0330

    feat: add Admin Pannel page

---
+++ src/components/PannelAdmin.jsx
@@ -0,0 +1,12 @@
+const PannelAdmin = () => {
+  return (
+    <>
+      <div className=" bg-pink2 min-h-screen ">
+        <h1 className="font-Vazir font-extrabold text-green3 text-2xl text-center py-5 ">
+          پنل مدیریتی
+        </h1>
+      </div>
+    </>
+  );
+};
+export default PannelAdmin;
