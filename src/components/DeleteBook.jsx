03d2c10dc4e49139d5ee77af006a425832813f2c
asma ezzati <asmaezzati2019@gmail.com>
2025-09-21 15:08:12 +0330

    styled delete books page in admin pannel

--- src/components/DeleteBook.jsx
+++ src/components/DeleteBook.jsx
@@ -1,2 +1,53 @@
-const DeleteBook = () => {};
+import { useSelector, useDispatch } from "react-redux";
+import { deleteBook, selectAll } from "../reducer/BookSlice";
+import { FaTrashAlt } from "react-icons/fa";
+
+const DeleteBook = () => {
+  const dispatch = useDispatch();
+  const books = useSelector(selectAll);
+
+  return (
+    <>
+      <div className="flex justify-center items-center flex-wrap mt-5 ">
+        {books.map((book) => (
+          <div
+            key={book.id}
+            className="w-[500px] border-2 border-green3 rounded-xl p-4 my-5 mx-auto flex"
+          >
+            <img
+              src={book.image}
+              alt={book.title}
+              className="w-1/2 m-2 rounded-xl"
+            ></img>
+            <div className="m-2 w-1/2 mt-5 ">
+              <h1 className="font-Vazir font-bold text-green3 py-2 text-center  ">
+                نام کتاب : {book.title}
+              </h1>
+              <h1 className="font-Vazir font-bold text-green3 py-2 text-center  ">
+                نویسنده: {book.author}
+              </h1>
+              <h1 className="font-Vazir font-bold text-green3 py-2 text-center  ">
+                سال : {book.year}
+              </h1>
+              <h1 className="font-Vazir font-bold text-green3 py-2 text-center  ">
+                ژانر : {book.genre}
+              </h1>
+              <h1 className="font-Vazir font-bold text-green3 py-2 text-center  ">
+                قیمت : {book.price}
+              </h1>
+
+              <button
+                className=" flex justify-center   w-full border-2 border-green3 rounded-xl  mt-8 px-2 py-3 font-Vazir text-white font-bold bg-green3
+              hover:bg-pink1 hover:border-2 hover:border-pink2 "
+              >
+                حذف کتاب
+                <FaTrashAlt size={20} />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 export default DeleteBook;
