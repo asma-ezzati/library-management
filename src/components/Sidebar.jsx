fade2bf92f33e95dd337d0f798b32f498e5207e1
asma ezzati <asmaezzati2019@gmail.com>
2025-09-21 15:30:21 +0330

    update sidebar button when clicked

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,18 +5,27 @@ import { IoMdExit } from "react-icons/io";
 const Sidebar = () => {
   return (
     <>
-      <div className=" w-[80px] h-screen bg-green3 m-5 rounded-xl flex flex-wrap flex-col p-5  ">
-        <Link to={"/admin/edititem"} className=" text-white my-4 mx-auto">
+      <div className=" w-[80px] h-[300px] bg-white m-5 rounded-xl flex flex-wrap flex-col p-5  ">
+        <Link
+          to={"/admin/edititem"}
+          className=" text-green2 my-4 mx-auto focus:text-pink1"
+        >
           <FaEdit size={30} />
         </Link>
-        <Link to={"/admin/additem"} className=" text-white my-4 mx-auto">
+        <Link
+          to={"/admin/additem"}
+          className="text-green2 my-4 mx-auto focus:text-pink1"
+        >
           <FaPlus size={30} />
         </Link>
-        <Link to={"/admin/deleteitem"} className=" text-white my-4 mx-auto">
+        <Link
+          to={"/admin/deleteitem"}
+          className="text-green2 my-4 mx-auto focus:text-pink1"
+        >
           <FaTrashAlt size={30} />
         </Link>
-        <Link to={"/"} className=" text-white my-4 mx-auto">
-          <IoMdExit size={30} />
+        <Link to={"/"} className="text-green2 my-4 mx-auto focus:text-pink1">
+          <IoMdExit size={38} />
         </Link>
       </div>
     </>
