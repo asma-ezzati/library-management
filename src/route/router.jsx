3a94f510043b1faabb8b913c98e156980376a376
asma ezzati <asmaezzati2019@gmail.com>
2025-09-22 18:49:11 +0330

    fixed bug in route issue

--- src/route/router.jsx
+++ src/route/router.jsx
@@ -23,7 +23,7 @@ export const router = createBrowserRouter([
     element: <BooksList />,
   },
   {
-    path: "/books/:booksId",
+    path: "/books/:bookId",
     element: <SingleBooksPage />,
   },
   {
