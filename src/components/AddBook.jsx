dc77c6f10b55bd8b2d31e04056acab454e280dfe
asma ezzati <asmaezzati2019@gmail.com>
2025-09-22 14:13:44 +0330

    feat submit form with dispatch and add books in admin pannel

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBooks } from "../reducer/BookSlice";
+
 const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
+  const [year, setYear] = useState();
+  const [genre, setGenre] = useState();
+  const [price, setPrice] = useState();
+  const [image, setImage] = useState();
+
+  const onChangeTitle = (e) => setTitle(e.target.value);
+  const onChangeAuthor = (e) => setAuthor(e.target.value);
+  const onChangeYear = (e) => setYear(e.target.value);
+  const onChangeGenre = (e) => setGenre(e.target.value);
+  const onChangePrice = (e) => setPrice(e.target.value);
+  const onChangeImage = (e) => setImage(e.target.value);
+
+  const handleSubmitForm = () => {
+    dispatch(
+      addBooks({
+        title: title,
+        author: author,
+        year: year,
+        genre: genre,
+        price: price,
+        image: `/images/${image}`,
+      })
+    );
+    setTitle(" ");
+    setAuthor(" ");
+    setYear(" ");
+    setGenre(" ");
+    setPrice(" ");
+    setImage(" ");
+  };
+
   return (
     <>
       <main className="w-[50%] mx-[220px] flex justify-center items-center bg-white p-8 m-5 rounded-xl border-2 border-pink1">
-        <form className="w-full flex flex-col items-center">
+        <form
+          onSubmit={handleSubmitForm}
+          className="w-full flex flex-col items-center"
+        >
           <label
             htmlFor="title"
             className="font-Vazir text-green3 font-bold text-lg p-2 text-center w-full"
@@ -10,6 +51,8 @@ const AddBook = () => {
             نام کتاب
           </label>
           <input
+            value={title}
+            onChange={onChangeTitle}
             id="title"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
@@ -22,6 +65,8 @@ const AddBook = () => {
             نویسنده
           </label>
           <input
+            value={author}
+            onChange={onChangeAuthor}
             id="author"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
@@ -34,6 +79,8 @@ const AddBook = () => {
             سال
           </label>
           <input
+            value={year}
+            onChange={onChangeYear}
             id="year"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
@@ -46,6 +93,8 @@ const AddBook = () => {
             ژانر
           </label>
           <input
+            value={genre}
+            onChange={onChangeGenre}
             id="genre"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
@@ -58,6 +107,8 @@ const AddBook = () => {
             قیمت
           </label>
           <input
+            value={price}
+            onChange={onChangePrice}
             id="price"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
@@ -70,6 +121,8 @@ const AddBook = () => {
             آدرس عکس
           </label>
           <input
+            value={image}
+            onChange={onChangeImage}
             id="image"
             type="text"
             className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
