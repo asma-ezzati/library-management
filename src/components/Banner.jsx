f796da560f955e9f5d5db4416dcbb1ecb55117ef
asma ezzati <asmaezzati2019@gmail.com>
2025-09-23 20:34:18 +0330

    refactor: extract searchBar to a separate Banner components

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import banner from "../assets/banner-pic.jpg";
+import BookSearch from "./BookSearch";
 
 const Banner = () => {
   return (
@@ -8,17 +9,7 @@ const Banner = () => {
           src={banner}
           className="w-full h-full object-contain blur-[2px] "
         ></img>
-        <div className="absolute inset-0 flex items-center justify-center ">
-          <form className="my-2  ">
-            <input
-              placeholder="جستجو"
-              className="w-[550px] h-[55px] bg-pink2 text-green3 font-Vazir  px-5 py-2 border-2 border-pink1 rounded-full 
-              transition-transform duration-1000
-              focus:outline-none focus:border-2 focus:border-pink1 focus:transition-transform  focus:duration-1000  
-              focus:scale-x-125  focus:-translate-y-5"
-            ></input>
-          </form>
-        </div>
+        <BookSearch />
       </div>
     </>
   );
