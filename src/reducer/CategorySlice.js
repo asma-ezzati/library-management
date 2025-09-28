d8ecf2c9f9007fd179adf010f2989fda3be7a284
asma ezzati <asmaezzati2019@gmail.com>
2025-09-28 22:22:35 +0330

    create new slice reducer for category

---
+++ src/reducer/CategorySlice.js
@@ -0,0 +1,74 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const categoryAdapter = createEntityAdapter({});
+const initialState = categoryAdapter.getInitialState({
+  status: "idle",
+});
+
+export const fetchCategory = createAsyncThunk(
+  "category/fetchCategory",
+  async () => {
+    try {
+      const response = await axios.get(" http://localhost:9000/category");
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const addCategory = createAsyncThunk(
+  "category/addCategory",
+  async () => {
+    try {
+      const response = await axios.post(" http://localhost:9000/category");
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const deleteCategory = createAsyncThunk(
+  "category/deleteCategory",
+  async (initialcatId) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:9000/category/${initialcatId}`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const CategorySlice = createSlice({
+  name: "category",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategory.fulfilled, (state, action) => {
+        categoryAdapter.setAll(state, action.payload);
+      })
+      .addCase(addCategory.fulfilled, (state, action) => {
+        categoryAdapter.addOne(state, action.payload);
+      })
+      .addCase(deleteCategory.fulfilled, (state, action) => {
+        categoryAdapter.removeOne(state, action.payload);
+      });
+  },
+});
+
+export default CategorySlice.reducer;
+
+export const {
+  selectAll: selectAllCat,
+  selectById: selectByCatIds,
+  selectIds: selectCatIds,
+} = categoryAdapter.getSelectors((state) => state.category);
