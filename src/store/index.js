281652c397007366fbb0c25c43ccdf6516360826
asma ezzati <asmaezzati2019@gmail.com>
2025-09-28 22:28:30 +0330

    add category reducer in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer, { fetchBooks } from "../reducer/BookSlice";
+import categoryReducer, { fetchCategory } from "../reducer/CategorySlice";
 
 export const store = configureStore({
   reducer: {
     books: booksReducer,
+    category: categoryReducer,
   },
 });
 
 store.dispatch(fetchBooks());
+store.dispatch(fetchCategory());
