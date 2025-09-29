d39945210237eeb906d5885baad105deb23150f4
asma ezzati <asmaezzati2019@gmail.com>
2025-09-29 20:11:44 +0330

    update edit book form for read genre and select

--- src/components/EditBookForm.jsx
+++ src/components/EditBookForm.jsx
@@ -2,34 +2,44 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import { editBook, selectById } from "../reducer/BookSlice";
+import { selectAllCat } from "../reducer/CategorySlice";
 
 const EditBookForm = () => {
   const { bookId } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const categories = useSelector(selectAllCat);
   const book = useSelector((state) => selectById(state, bookId));
   const [title, setTitle] = useState(book.title);
   const [author, setAuthor] = useState(book.author);
   const [year, setYear] = useState(book.year);
-  const [genre, setGenre] = useState(book.genre);
+  const [genreId, setGenreId] = useState(book.genre);
   const [price, setPrice] = useState(book.price);
   const [image, setImage] = useState(book.image);
 
   const onChangeTitle = (e) => setTitle(e.target.value);
   const onChangeAuthor = (e) => setAuthor(e.target.value);
   const onChangeYear = (e) => setYear(e.target.value);
-  const onChangeGenre = (e) => setGenre(e.target.value);
+  const onChangeGenre = (e) => setGenreId(e.target.value);
   const onChangePrice = (e) => setPrice(e.target.value);
   const onChangeImage = (e) => setImage(e.target.value);
 
   const handleSubmitForm = () => {
     dispatch(
-      editBook({ id: bookId, title, author, year, genre, price, image })
+      editBook({
+        id: bookId,
+        title,
+        author,
+        year,
+        genre: genreId,
+        price,
+        image,
+      })
     );
     setTitle(" ");
     setAuthor(" ");
     setYear(" ");
-    setGenre(" ");
+    setGenreId(" ");
     setPrice(" ");
     setImage(" ");
     navigate("/admin/edititem");
@@ -90,13 +100,20 @@ const EditBookForm = () => {
           >
             ژانر
           </label>
-          <input
-            value={genre}
+          <select
+            value={genreId}
             onChange={onChangeGenre}
             id="genre"
             type="text"
-            className="w-full p-2 rounded-lg font-Vazir font-semibold text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
-          />
+            className="w-full p-2 rounded-lg font-Vazir text-green3 m-2 bg-white border-2 border-pink2 focus:outline-pink1"
+          >
+            <option value="">انتخاب ژانر</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.genre}
+              </option>
+            ))}
+          </select>
 
           <label
             htmlFor="price"
