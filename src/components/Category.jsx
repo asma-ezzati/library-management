0f8a221e00d2a50817001bdbd6f014d9de8667e5
asma ezzati <asmaezzati2019@gmail.com>
2025-09-30 17:20:55 +0330

    implement category part in main page

---
+++ src/components/Category.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { selectAllCat } from "../reducer/CategorySlice";
+import { Link } from "react-router-dom";
+
+const Category = () => {
+  const categories = useSelector(selectAllCat);
+  return (
+    <>
+      <div className="bg-pink2 h-[720px] min-h-screen py-8" id="category">
+        <div className="flex justify-between">
+          <h1 className="text-green3 font-Vazir font-bold text-2xl py-5 text-right mr-16 ">
+            دسته بندی کتاب ها
+          </h1>
+        </div>
+        <div className="flex justify-around flex-row ">
+          {categories.map((cat) => (
+            <Link to={`/genre/${cat.id}`} key={cat.id}>
+              {cat.genre}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Category;
