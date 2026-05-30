93f9b358f6e8ccb52817eddde86e6c0eacb41c87
asma ezzati <asmaezzati2019@gmail.com>
2026-05-30 20:13:33 +0330

    fix bug in map

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -24,37 +24,35 @@ const Books = () => {
       </div>
       <div className="flex justify-around flex-row ">
         {lastThree.map((book) => (
-          <>
-            <div
-              key={book.id}
-              className=" relative w-[300px] border-2 rounded-2xl  border-green2 bg-green1  mx-5 my-3 duration-200
+          <div
+            key={book.id}
+            className=" relative w-[300px] border-2 rounded-2xl  border-green2 bg-green1  mx-5 my-3 duration-200
             hover:transition-transform hover:-translate-y-2 hover:duration-200   h-100 overflow-hidden  shadow-2xl cursor-pointer group"
-            >
-              <Link to={`/books/${book.id}`}>
-                <img
-                  className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 rounded-2xl"
-                  src={book.image}
-                  alt={book.title}
-                ></img>
+          >
+            <Link to={`/books/${book.id}`}>
+              <img
+                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 rounded-2xl"
+                src={book.image}
+                alt={book.title}
+              ></img>
 
-                <div className=" bg-pink2 opacity-0  absolute inset-0 bg-black bg-opacity-70 flex flex-col justify-center items-center  text-center p-4  group-hover:opacity-100 transition-opacity duration-300">
-                  <h2 className=" font-Vazir text-center text-green3 font-bold text-2xl py-3 pt-6  ">
-                    {book.title}
-                  </h2>
-                  <hr className="border-green3 border-t-2 w-1/2 mx-auto rounded-full"></hr>
-                  <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-                    {book.author}
-                  </h4>
-                  <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-                    {book.genreName}
-                  </h4>
-                  <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
-                    قیمت : {book.price}
-                  </h4>
-                </div>
-              </Link>
-            </div>
-          </>
+              <div className=" bg-pink2 opacity-0  absolute inset-0 bg-black bg-opacity-70 flex flex-col justify-center items-center  text-center p-4  group-hover:opacity-100 transition-opacity duration-300">
+                <h2 className=" font-Vazir text-center text-green3 font-bold text-2xl py-3 pt-6  ">
+                  {book.title}
+                </h2>
+                <hr className="border-green3 border-t-2 w-1/2 mx-auto rounded-full"></hr>
+                <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                  {book.author}
+                </h4>
+                <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                  {book.genreName}
+                </h4>
+                <h4 className=" font-Vazir text-center text-green3 font-bold py-2 ">
+                  قیمت : {book.price}
+                </h4>
+              </div>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
